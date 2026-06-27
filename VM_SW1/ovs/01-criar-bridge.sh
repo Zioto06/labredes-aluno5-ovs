ebf0a5ab41e6025f56203d4b7b8102d714c05007
Luis_Zioto <151163500+Zioto06@users.noreply.github.com>
2026-06-27 17:44:27 -0300

    Update 01-criar-bridge.sh

--- VM_SW1/ovs/01-criar-bridge.sh
+++ VM_SW1/ovs/01-criar-bridge.sh
@@ -1,16 +1,5 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/bash
 
-# Criação da bridge principal do Open vSwitch no VM_SW1.
-# A bridge br-int será responsável pela comutação L2 das VLANs.
+# Criação da bridge principal do VM_SW1
 
-echo "[VM_SW1] Criando bridge br-int..."
-
-sudo systemctl enable openvswitch-switch
-sudo systemctl start openvswitch-switch
-
-sudo ovs-vsctl --may-exist add-br br-int
-
-echo "[VM_SW1] Bridge br-int criada/verificada com sucesso."
-
-sudo ovs-vsctl show
+sudo ovs-vsctl add-br br-int
