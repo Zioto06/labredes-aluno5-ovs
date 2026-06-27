2dcdc49d5c03fa972c19ed43827c9cc664c8a970
Luis_Zioto <151163500+Zioto06@users.noreply.github.com>
2026-06-27 17:45:08 -0300

    Update 03-configurar-portas-access.sh

--- VM_SW1/ovs/03-configurar-portas-access.sh
+++ VM_SW1/ovs/03-configurar-portas-access.sh
@@ -1,37 +1,33 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/bash
 
-# Configuração das portas access do VM_SW1.
-# Cada interface é associada a uma VLAN específica.
+# Configuração das portas access do VM_SW1
 
-BRIDGE="br-int"
+# ens3 - VM9 - VLAN 10
+sudo ip link set ens3 up
+sudo ovs-vsctl --may-exist add-port br-int ens3
+sudo ovs-vsctl clear Port ens3 trunks
+sudo ovs-vsctl set Port ens3 vlan_mode=access tag=10
 
-echo "[VM_SW1] Configurando portas access..."
+# ens4 - VM3 - VLAN 40
+sudo ip link set ens4 up
+sudo ovs-vsctl --may-exist add-port br-int ens4
+sudo ovs-vsctl clear Port ens4 trunks
+sudo ovs-vsctl set Port ens4 vlan_mode=access tag=40
 
-config_access_port() {
-  local iface="$1"
-  local vlan="$2"
-  local descricao="$3"
+# ens6 - VM_DEV - VLAN 50
+sudo ip link set ens6 up
+sudo ovs-vsctl --may-exist add-port br-int ens6
+sudo ovs-vsctl clear Port ens6 trunks
+sudo ovs-vsctl set Port ens6 vlan_mode=access tag=50
 
-  echo "[VM_SW1] Configurando $iface como access VLAN $vlan ($descricao)..."
+# ens7 - VM_USER1 - VLAN 60
+sudo ip link set ens7 up
+sudo ovs-vsctl --may-exist add-port br-int ens7
+sudo ovs-vsctl clear Port ens7 trunks
+sudo ovs-vsctl set Port ens7 vlan_mode=access tag=60
 
-  sudo ip link set "$iface" up
-
-  sudo ovs-vsctl --may-exist add-port "$BRIDGE" "$iface"
-
-  # Remove configuração anterior de trunk, se existir
-  sudo ovs-vsctl clear Port "$iface" trunks || true
-
-  # Define a porta como access na VLAN informada
-  sudo ovs-vsctl set Port "$iface" vlan_mode=access tag="$vlan"
-}
-
-config_access_port ens3  10 "VM9 - Autenticação"
-config_access_port ens4  40 "VM3 - Descoberta de Hosts"
-config_access_port ens6  50 "VM_DEV - Estação de Desenvolvimento"
-config_access_port ens7  60 "VM_USER1 - Usuários"
-config_access_port ens14 40 "VM4 - Gerência/Captura"
-
-echo "[VM_SW1] Portas access configuradas com sucesso."
-
-sudo ovs-vsctl show
+# ens14 - VM4 - VLAN 40
+sudo ip link set ens14 up
+sudo ovs-vsctl --may-exist add-port br-int ens14
+sudo ovs-vsctl clear Port ens14 trunks
+sudo ovs-vsctl set Port ens14 vlan_mode=access tag=40
